194913b42b6c3344f83176ca1f5d87800b62aa9a
khairunoyshee-rgb <khairunoyshee@gmail.com>
2025-12-15 00:46:52 +0600

    Add header file for string subtraction function

---
+++ substraction.h
@@ -0,0 +1,7 @@
+#ifndef SUBTRACTION_H
+#define SUBTRACTION_H
+
+// Function declaration: tells the compiler about the subtraction function
+char* string_subtraction(const char* word1, const char* word2);
+
+#endif
