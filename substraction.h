b1837ed953ba8393b4e8bcaad674cad907afc267
khairunoyshee-rgb <khairunoyshee@gmail.com>
2025-12-15 01:01:42 +0600

    Clean up comments in substraction.h
    Removed unnecessary comments from substraction.h

--- substraction.h
+++ substraction.h
@@ -1,7 +1,4 @@
 #ifndef SUBTRACTION_H
 #define SUBTRACTION_H
-
-// Function declaration: tells the compiler about the subtraction function
 char* string_subtraction(const char* word1, const char* word2);
-
 #endif
